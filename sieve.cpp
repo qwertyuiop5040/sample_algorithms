f2129afb824fbb49db9394b854cc020f2f8424ae
qwertyuiop5040 <hwang00@umail.ucsb.edu>
2017-05-24 03:42:10 -0700

    Update sieve.cpp

--- sieve.cpp
+++ sieve.cpp
@@ -12,7 +12,7 @@ vector<int> sieve(int n){
 	p.push_back(3);
 	for(int i=5;i<n;i+=2){
 		bool good=true;
-		for(int j=0;j<p.size()&&p[j]*p[j]<i;j++){
+		for(int j=0;j<p.size()&&p[j]*p[j]<=i;j++){
 			if(i%p[j]==0){
 				good=false;
 				break;
